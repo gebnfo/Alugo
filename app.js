dc2f9f58358a7f646361e9bb9241a841e3294675
gebnfo <ge.georgesantos@hotmail.com>
2020-04-30 02:20:05 -0300

    teste melhora da nota

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js')
-    .then((reg) => console.log('service worker funcionando',reg))
-    .catch((err) => console.log('erro no service worker'),err);
-
-}
+if ('serviceWorker' in navigator) {
+    // Registre um service worker hospeado na raiz do
+    // site usando um escopo mais restritivo.
+    navigator.serviceWorker.register('sw.js', {scope: './'}).then(function(registration) {
+      console.log('Service worker registrado com sucesso:', registration);
+    }).catch(function(error) {
+      console.log('Service worker falhou ao registrar:', error);
+    });
+  } else {
+    console.log('Service workers não é suportado pelo navegador!.');
+  }
